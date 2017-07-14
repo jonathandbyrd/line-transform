9513ffa9d276fa37600f103976016da3a1a8c5a5
Jonathan D Byrd <jonathan.d.byrd@live.com>
2017-07-14 00:15:29 -0400

    Added filesindir. Correctly async processes file dir

--- app/filesindir.js
+++ app/filesindir.js
@@ -55,10 +55,6 @@ if (recordType == undefined || recordType.length <= 0) {
     //load the data dictionary
     var dd = require(ddPath);
 
-    var lr;
-    var out;
-    var totalrec = 0;
-    var goodrec = 0;
 
     fs.readdir(dataDirectory, function(err, files) {
       if (err) {
@@ -75,18 +71,58 @@ if (recordType == undefined || recordType.length <= 0) {
         var dataFile = path.join(dataDirectory, file)
         var outputFile = path.join(outputDirectory, file + ".csv");
 
-        lr = new LineByLineReader(dataFile);
-        out = fs.createWriteStream(outputFile, "utf8");
+        var lr = new LineByLineReader(dataFile);
+        var out = fs.createWriteStream(outputFile, "utf8");
 
-        //console.log("df=" + dataFile);
-        //console.log("of=" + outputFile);
+        var totalrec = 0;
+        var goodrec = 0;
 
         lr.on('error', function (err) {
           // 'err' contains error object
           console.log("Error: " + err);
         });
 
-        lr.on('line', processLine);
+        lr.on('line', function(line) {
+          totalrec++;
+          //if line is empty get out
+          if (line.length <= 0) return;
+
+          //determine if its a record we can exclude; no eupid
+          //{"EUPID"}
+          if (line.substr(keyMap.eupidIndex[0], keyMap.eupidIndex[1]).trim().length <= 0) return;
+
+          goodrec++;
+
+          //need to remove double quotes
+          //we'll replace with a space to preserve
+          //the nature of the fixed-width file
+          var cleanLine = line.replace(/"/g, " ");
+
+          //array to hold columns of data
+          var data = [];
+
+          //chop up the data line by field
+          for(var x in dd.fields) {
+            var field = dd.fields[x];
+            data[x] = cleanLine.substr((field.startPos - 1), field.length).trim();
+          }
+
+          //generate our key
+          var rowKey = cleanLine.substr(keyMap.eupidIndex[0], keyMap.eupidIndex[1]).trim() //eupid
+                     + cleanLine.substr(keyMap.admitDateIndex[0], keyMap.admitDateIndex[1]).trim() //AdmissionDate
+                     + cleanLine.substr(keyMap.facilityIDIndex[0], keyMap.facilityIDIndex[1]).trim(); //FacilityID
+
+          var hashedKey = crypto.createHash("md5").update(rowKey).digest("hex");
+
+          //we'll append the hashed Key as the
+          //first column of data to the data row
+          var finalLine = `"${hashedKey}","${data.join("\",\"")}"`;
+
+          //output each clean line of data
+          //here we'll remove two empty double quotes
+          //with simply an empty string
+          out.write(`${finalLine.replace(/""/g, "")}${eol}`);
+        });
 
         lr.on('end', function () {
           // All lines are read, file is closed now.
@@ -95,49 +131,3 @@ if (recordType == undefined || recordType.length <= 0) {
         });
       });
     });
-
-
-    function getMD5Hash(data) {
-      return crypto.createHash("md5").update(data).digest("hex");
-    }
-
-    function processLine(line) {
-      totalrec++;
-      //if line is empty get out
-      if (line.length <= 0) return;
-
-      //determine if its a record we can exclude; no eupid
-      //{"EUPID"}
-      if (line.substr(keyMap.eupidIndex[0], keyMap.eupidIndex[1]).trim().length <= 0) return;
-
-      goodrec++;
-
-      //need to remove double quotes
-      //we'll replace with a space to preserve
-      //the nature of the fixed-width file
-      var cleanLine = line.replace(/"/g, " ");
-
-      var data = [];
-
-      //generate our key
-      var rowKey = cleanLine.substr(keyMap.eupidIndex[0], keyMap.eupidIndex[1]).trim() //eupid
-                 + cleanLine.substr(keyMap.admitDateIndex[0], keyMap.admitDateIndex[1]).trim() //AdmissionDate
-                 + cleanLine.substr(keyMap.facilityIDIndex[0], keyMap.facilityIDIndex[1]).trim(); //FacilityID
-
-      var hashedKey = getMD5Hash(rowKey);
-
-      //chop up the data line by field
-      for(var x in dd.fields) {
-        var field = dd.fields[x];
-        data[x] = cleanLine.substr((field.startPos - 1), field.length).trim();
-      }
-
-      //we'll append the hashed Key as the
-      //first column of data to the data row
-      var finalLine = `"${hashedKey}","${data.join("\",\"")}"`;
-
-      //output each clean line of data
-      //here we'll remove two empty double quotes
-      //with simply an empty string
-      out.write(`${finalLine.replace(/""/g, "")}${eol}`);
-    }
